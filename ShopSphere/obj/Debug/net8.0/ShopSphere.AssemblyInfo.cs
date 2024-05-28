81eb44f92010cf6d4d24366f682d603d7901ea71
DENISPC\denis <denis.halimaj11@gmail.com>
2024-05-29 00:44:45 +0200

    Applied new migrations

--- ShopSphere/obj/Debug/net8.0/ShopSphere.AssemblyInfo.cs
+++ ShopSphere/obj/Debug/net8.0/ShopSphere.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopSphere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c40e36cd35ee202ce185ebc1fb442ffc2db6d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopSphere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopSphere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
